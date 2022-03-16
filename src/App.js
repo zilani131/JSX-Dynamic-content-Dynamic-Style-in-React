68fac54ca848dbf42ab855aab02ef443f3f8e595
zilani131 <zilani.cuet@gmail.com>
2022-03-16 11:49:27 +0600

    creating bascic component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-
+const name='zilani'
+const player={name:"afridi",age:56}
+const playerStyle={
+  color:"blue",
+  backgroundColor:"white"
+}
+const player1={name:'Sakib ul hasan',profession:'Cricketer'};
+const player2={name:'Afridid',profession:"Cricketer"}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='edit'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <h2>Another component</h2>
+      <Person2></Person2>
+      <Person2></Person2>
+      <Person2></Person2>
     </div>
   );
 }
 
+function Person(){
+  return (
+  <div className='player'>
+    <h1>{player1.name}</h1>
+    <p>Profession:{player1.profession}</p>
+  </div>
+  );
+}
+function Person2(){
+  return (
+  <div className='player2'>
+    <h1>{player2.name}</h1>
+    <p>Profession:{player2.profession}</p>
+  </div>
+  );
+}
 export default App;
